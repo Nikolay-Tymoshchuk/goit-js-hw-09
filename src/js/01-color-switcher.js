453ea2a8317bb49917938ad6059b2d2e9f7173be
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-04-19 19:42:07 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const INTERVAL_TIME = 1000;
+let intervalID = null;
+const refs = {
+    startBtn: document.body.querySelector('[data-start]'),
+    stopBtn: document.body.querySelector('[data-stop]'),
+    body: document.body
+};
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    intervalID = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, INTERVAL_TIME);
+    this.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStopBtnClick() {
+    clearInterval(intervalID);
+    refs.startBtn.disabled = false;
+}
