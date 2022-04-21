42823810fa8e64316fd73bea72163d9d5b967ee5
Nikolay-Tymoshchuk <saintnikfreak@gmail.com>
2022-04-21 11:46:20 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,4 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Report } from 'notiflix/build/notiflix-report-aio';
-import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { Block } from 'notiflix/build/notiflix-block-aio';
 
 const refs = {
   form: document.querySelector('.form'),
